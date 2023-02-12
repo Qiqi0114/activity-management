e45313a41687e5e0c8c63a41d020e2247f4c271c
七七 <1772014415@qq.com>
2023-02-12 22:58:17 +0800

    登录静态页;安装sass 因为css不支持嵌套

--- activity-management/src/pages/login/login.tsx
+++ activity-management/src/pages/login/login.tsx
@@ -1,7 +1,40 @@
+import { Button, Card, Form, Input } from "antd"
+import "./login.scss"
+import { ILoginParams } from "./login.type";
 export default function Login () {
+    const login = (values:ILoginParams) =>{
+        console.log(values);
+    }
     return (
-        <div>
-            <div>登录</div>
+        <div id="login">
+            <Card  style={{ width: 300 }}>
+                <h2 className="title">七七活动管理</h2>
+                <Form
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    onFinish={login}
+                >
+                    <Form.Item
+                    label="用户名"
+                    name="username"
+                    rules={[{ required: true, message: '请输入用户名!' }]}
+                    >
+                    <Input />
+                    </Form.Item>
+                    <Form.Item
+                    label="密码"
+                    name="password"
+                    rules={[{ required: true, message: '请输入密码!' }]}
+                    >
+                    <Input.Password />
+                    </Form.Item>
+                    <div id="login-btn">
+                        <Button type="primary" htmlType="submit">登录</Button>
+                    </div>
+                </Form>
+            </Card>
+
         </div>
     )
 }
---
+++ activity-management/src/pages/login/login.type.ts
@@ -0,0 +1,7 @@
+/*  
+    登录传的参数
+*/
+export interface ILoginParams {
+    username:string
+    password:string
+}
